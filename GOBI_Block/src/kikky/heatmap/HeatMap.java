86ececcdff0019421a066f0d8fc45ade93638a83
Klaudia Adamowicz <adamowicz.cip.ifi.lmu.de>
2017-03-16 17:38:21 +0100

    non 0.0.0.0.0.1

---
+++ GOBI_Block/src/kikky/heatmap/HeatMap.java
@@ -0,0 +1,9 @@
+package kikky.heatmap;
+
+import java.util.ArrayList;
+
+public class HeatMap {
+	private String R_path = "/home/proj/biosoft/software/R/R-3.3.0/bin/Rscript";
+	private ArrayList<String> labels = new ArrayList<>();
+	private String file_path;
+}
